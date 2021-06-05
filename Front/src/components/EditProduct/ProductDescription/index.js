1bdf21053aaedc031138c8974e1c1de8b963b387
Leonardo a Luz <leoluzprog@gmail.com>
2021-06-05 12:00:08 -0300

    ckeditor inserido e adaptado para edição de produto, porém ainda falta integrar com o formik.

--- Front/src/components/EditProduct/ProductDescription/index.js
+++ Front/src/components/EditProduct/ProductDescription/index.js
@@ -1,17 +1,73 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+//import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { ButtonFlat, Row } from '../../../globalStyleds';
 import { colorTheme } from '../../../theme';
 import { DescricaoPgProduto, SaveOrDiscard } from './styles';
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
 
 export function ProductDescription(props) {
+
+    const editorConfiguration = {
+        toolbar: {
+            items: [
+                'heading',
+                '|',
+                'bold',
+                'italic',
+                'fontFamily',
+                'fontSize',
+                'fontColor',
+                'highlight',
+                'alignment',
+                'bulletedList',
+                'numberedList',
+                '|',
+                'outdent',
+                'indent',
+                '|',
+                'link',
+                'imageInsert',
+                'imageUpload',
+                'blockQuote',
+                'insertTable',
+                'mediaEmbed',
+                'undo',
+                'redo'
+            ]
+        },
+        language: 'pt-br',
+        image: {
+            toolbar: [ 'imageTextAlternative', '|', 'imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight' ],
+
+            styles: [
+                // This option is equal to a situation where no style is applied.
+                'full',
+
+                // This represents an image aligned to the left.
+                'alignLeft',
+
+                // This represents an image aligned to the right.
+                'alignRight'
+            ]
+        },
+        table: {
+            contentToolbar: [
+                'tableColumn',
+                'tableRow',
+                'mergeTableCells'
+            ]
+        },
+        licenseKey: '',
+    };
 
     return (
         <DescricaoPgProduto>
             <div><h3 >Descrição do produto</h3></div>
+            dsdd
             <CKEditor
-                editor={ClassicEditor}
+                editor={Editor}
                 data={props.produto.descricao}
+                config={editorConfiguration}
             />
             <SaveOrDiscard>
                 <ButtonFlat bgColor={colorTheme.warning()}>Descartar alterações</ButtonFlat>
