3eec390326f87a34c30117abeae1dab045b7fc8a
Leonardo a Luz <leoluzprog@gmail.com>
2021-06-18 00:13:58 -0300

    FEATURE: deletar produto

--- Front/src/components/EditProduct/Options/index.js
+++ Front/src/components/EditProduct/Options/index.js
@@ -1,13 +1,30 @@
 import { Options_ } from "./styles";
 import { ButtonFlat } from "../../../globalStyleds";
 import { colorTheme } from "../../../theme";
+import { deleteProduct } from "../../../store/actions/produto";
+import { useHistory } from "react-router";
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
 
-export const Options = ({ draftStatus, saveChanges, discardChanges }) => {
+export const Options__ = ({ product, draftStatus, saveChanges, discardChanges, deleteProduct }) => {
+
+    const history = useHistory();
+   
     return (
         <Options_>
             <ButtonFlat bgColor={colorTheme.neutral()} onClick={discardChanges}>Voltar</ButtonFlat>
-            <ButtonFlat bgColor={colorTheme.warning()}  onClick={discardChanges}>Deletar</ButtonFlat>
+            <ButtonFlat bgColor={colorTheme.warning()}  onClick={()=>deleteProduct(product, ()=>history.push('/produtos/'))}>Deletar</ButtonFlat>
             <ButtonFlat disabled={draftStatus !== 'modified'} onClick={saveChanges} >Salvar</ButtonFlat>
         </Options_>
     )
 }
+
+const mapStateToProps = store => ({
+
+})
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ deleteProduct }, dispatch);
+
+
+export const Options = connect(mapStateToProps, mapDispatchToProps)(Options__);
